6ab6f718e739777e66c288f635a32d3f1c62e404
satel31 <blokhnina.tatiana@yandex.ru>
2023-03-09 22:35:08 +0300

    Added tests. Coverage 100%

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,27 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+
+@pytest.fixture
+def laptop():
+    return Item("Ноутбук", 20000, 5)
+
+def test_item_init(laptop):
+    """Test of initialization of the class Item"""
+    assert laptop.name == "Ноутбук"
+    assert laptop.price == 20000
+    assert laptop.quantity == 5
+    assert len(Item.all) == 1
+
+def test_item_calculate_total_price(laptop):
+    """Test of the method calculate_total_price"""
+    assert laptop.calculate_total_price() == 100000
+
+def test_item_apply_discount(laptop):
+    """"Test of the method apply_discount"""
+    laptop.apply_discount()
+    assert laptop.price == 20000
+
+
+
+
