07d21014d3883390ecc0c30f75eba6d06243426f
satel31 <blokhnina.tatiana@yandex.ru>
2023-03-23 23:38:00 +0300

    Added docstrings

--- src/phone.py
+++ src/phone.py
@@ -1,7 +1,17 @@
 from src.item import Item
 
 class Phone(Item):
-    def __init__(self, name: str, price: float, quantity: int, number_of_sim: int):
+    """
+    Класс для представления товара типа телефон в магазине.
+    """
+    def __init__(self, name: str, price: float, quantity: int, number_of_sim: int) -> None:
+        """
+        Создание экземпляра класса phone, родительский класс item.
+        :param name: Название товара.
+        :param price: Цена за единицу товара.
+        :param quantity: Количество товара в магазине.
+        :param number_of_sim: количество поддерживаемых сим-карт
+        """
         super().__init__(name, price, quantity)
         self.__number_of_sim = number_of_sim
 
@@ -14,17 +24,23 @@ class Phone(Item):
     def number_of_sim(self, new_number: int) -> None:
         """Задаёт новое количество поддерживаемых сим-карт"""
         if isinstance(new_number, int) and new_number > 0:
-            self.__new_number = new_number
+            self.__number_of_sim = new_number
         else:
             raise ValueError("Количество физических SIM-карт должно быть целым числом больше нуля.")
 
-    def __add__(self, other):
+    def __add__(self, other: Item) -> int | str:
+        """Cложение по количеству товара в магазине классов Phone и Item"""
         if isinstance(other, Item):
             return self.quantity + other.quantity
+        else:
+            return f'Складывать можно только экземпляры Phone и Item'
 
-    def __radd__(self, other):
+    def __radd__(self, other:Item) -> int | str:
+        """Cложение по количеству товара в магазине классов Item и Phone"""
         if isinstance(other, Item):
             return other.quantity + self.quantity
+        else:
+            return f'Складывать можно только экземпляры Phone и Item'
 
     def __repr__(self) -> str:
         return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity}, {self.__number_of_sim})"
